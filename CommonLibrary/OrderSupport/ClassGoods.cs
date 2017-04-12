a5a05fd44fe9b6c2ade074bb33242317d7d6fa78
HSL <HSL@HSL-PC>
2017-04-12 15:18:36 +0800

    提供一个订单类业务的基础类的建设

---
+++ CommonLibrary/OrderSupport/ClassGoods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonLibrary
+{
+    /********************************************************************************
+     * 
+     *     订单类的商品类，是单个的商品对象
+     * 
+     * 
+     * 
+     * 
+     ********************************************************************************/
+
+    /// <summary>
+    /// 订单的商品类
+    /// </summary>
+    public class ItemGoods
+    {
+        /// <summary>
+        /// 商品的编号，也可以是条形码编号
+        /// </summary>
+        public string ItemCode { get; set; } = string.Empty;
+        /// <summary>
+        /// 商品的名称
+        /// </summary>
+        public string ItemName { get; set; } = string.Empty;
+        /// <summary>
+        /// 购买商品的数量
+        /// </summary>
+        public int ItemCount { get; set; } = 1;
+        /// <summary>
+        /// 商品的单个的基础价格
+        /// </summary>
+        public double ItemSinglePrice { get; set; }
+
+
+        public double GetNormalPrice()
+        {
+            if (ItemCount >= 0)
+                return ItemSinglePrice * ItemCount;
+            else throw new Exception("数量错误");
+        }
+        /// <summary>
+        /// 根据具体的优惠条件来获取最终的价格，有可能来自会员的固定折扣，也有可能来自活动折扣
+        /// </summary>
+        /// <param name="method"></param>
+        /// <returns></returns>
+        public double GetPayPrice(Func<double, int, double> method)
+        {
+            return method.Invoke(ItemSinglePrice, ItemCount);
+        }
+    }
+}
---
+++ CommonLibrary/OrderSupport/ClassOrder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonLibrary
+{
+    /*****************************************************************
+     * 
+     *    订单类，本系统提供一个标准的订单设计准则代码 
+     * 
+     * 
+     * 
+     * 
+     *****************************************************************/
+
+
+    public class Order
+    {
+        /// <summary>
+        /// 订单的状态，0：创建，1：被取消，2：支付成功。
+        /// </summary>
+        public int Status { get; set; } = 0;
+        /// <summary>
+        /// 订单的流水号
+        /// </summary>
+        public string OrderNumber { get; set; } = string.Empty;
+        /// <summary>
+        /// 订单的生成时间，应该使用服务器的时间
+        /// </summary>
+        public DateTime CreateTime { get; set; } = DateTime.Now;
+        /// <summary>
+        /// 订单的会员账号，如果为空，说明不是会员请求的
+        /// </summary>
+        public string VIPNumber { get; set; } = string.Empty;
+        /// <summary>
+        /// 支付方式，有可能现金，银行汇款，支付宝，微信等等
+        /// </summary>
+        public int PaymentWay { get; set; } = 0;
+        /// <summary>
+        /// 支付时间，可用来统计分析每个单子的滞留时间
+        /// </summary>
+        public DateTime PaymentTime { get; set; } = DateTime.Now;
+        
+
+
+        /// <summary>
+        /// 获取所有商品普通的金额总和
+        /// </summary>
+        /// <returns></returns>
+        public double GetNormalPrices()
+        {
+            return 0d;
+        }
+        /// <summary>
+        /// 获取所有商品最后付款金额，减去了会员价或是活动价之后的金额
+        /// </summary>
+        /// <returns></returns>
+        public double GetPayPrices()
+        {
+            return 0d;
+        }
+
+    }
+}
