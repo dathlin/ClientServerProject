7873640ede2966235180769508c65aab85ee7790
dathlin <hsl200909@163.com>
2017-07-11 10:30:46 +0800

    登录界面设计

--- 软件系统客户端Wpf/LoginWindow.xaml.cs
+++ 软件系统客户端Wpf/LoginWindow.xaml.cs
@@ -44,6 +44,31 @@ namespace 软件系统客户端Wpf
             TextBlockSoftName.Text = CommonLibrary.Resource.StringResouce.SoftName;
             TextBlockSoftVersion.Text = UserClient.CurrentVersion.ToString();
             TextBlockSoftCopyright.Text = $"本软件著作权归{CommonLibrary.Resource.StringResouce.SoftCopyRight}所有";
+
+            UserClient.JsonSettings.FileSavePath = AppDomain.CurrentDomain.BaseDirectory + @"\JsonSettings.txt";
+            UserClient.JsonSettings.LoadByFile();
+
+            if ((DateTime.Now - UserClient.JsonSettings.LoginTime).TotalDays < 7)
+            {
+                //加载数据
+                NameTextBox.Text = UserClient.JsonSettings.LoginName ?? "";
+                PasswordBox.Password = UserClient.JsonSettings.Password ?? "";
+                Remember.IsChecked = UserClient.JsonSettings.Password != "";
+            }
+            //初始化输入焦点
+            if (UserClient.JsonSettings.Password != "") LoginButton.Focus();
+            else if (UserClient.JsonSettings.LoginName != "") PasswordBox.Focus();
+            else NameTextBox.Focus();
+        }
+
+        private void NameTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            PasswordBox.Focus();
+        }
+
+        private void PasswordBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            Button_Click(null, new RoutedEventArgs());
         }
     }
 }
