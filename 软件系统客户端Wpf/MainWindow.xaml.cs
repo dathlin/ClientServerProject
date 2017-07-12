6d79df3582e441784e331a2304a9c6d79d139bed
dathlin <hsl200909@163.com>
2017-07-12 09:26:20 +0800

    主界面添加了一些基础菜单，并未关联事件

--- 软件系统客户端Wpf/MainWindow.xaml.cs
+++ 软件系统客户端Wpf/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CommonLibrary;
+using HslCommunication.BasicFramework;
+using HslCommunication.Enthernet;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -53,5 +57,152 @@ namespace 软件系统客户端Wpf
         {
             InitializeComponent();
         }
+
+
+
+
+
+        #region 异步网络块
+
+        private Net_Socket_Client net_socket_client = new Net_Socket_Client();
+
+        private void Net_Socket_Client_Initialization()
+        {
+            try
+            {
+                net_socket_client.KeyToken = CommonHeadCode.KeyToken;//新增的身份令牌
+                net_socket_client.EndPointServer = new System.Net.IPEndPoint(
+                    System.Net.IPAddress.Parse(UserClient.ServerIp),
+                    CommonLibrary.CommonLibrary.Port_Main_Net);
+                net_socket_client.ClientAlias = $"{UserClient.UserAccount.UserName} ({UserClient.UserAccount.Factory})";//标记客户端在线的名称
+                net_socket_client.ClientStart();
+            }
+            catch (Exception ex)
+            {
+                SoftBasic.ShowExceptionMessage(ex);
+            }
+        }
+        /// <summary>
+        /// 接收到服务器的字节数据的回调方法
+        /// </summary>
+        /// <param name="state">网络连接对象</param>
+        /// <param name="customer">用户自定义的指令头，用来区分数据用途</param>
+        /// <param name="data">数据</param>
+        private void Net_socket_client_AcceptString(AsyncStateOne state, int customer, string data)
+        {
+            if (customer == CommonHeadCode.MultiNetHeadCode.弹窗新消息)
+            {
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    FormPopup fpp = new FormPopup(data, System.Drawing.Color.DodgerBlue, 10000);
+                    fpp.Show();
+                }));
+            }
+            else if (customer == CommonHeadCode.MultiNetHeadCode.总在线信息)
+            {
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    //listBox1.DataSource = data.Split('#');
+                }));
+            }
+            else if (customer == CommonHeadCode.MultiNetHeadCode.关闭客户端)
+            {
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    Close();
+                }));
+            }
+            else if (customer == CommonHeadCode.SimplifyHeadCode.更新公告)
+            {
+                //此处应用到了同步类的指令头
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    UserClient.Announcement = data;
+                    //label_Announcement.Text = data;
+                    FormPopup fpp = new FormPopup(data, System.Drawing.Color.DodgerBlue, 10000);
+                    fpp.Show();
+                }));
+            }
+            else if (customer == CommonHeadCode.MultiNetHeadCode.初始化数据)
+            {
+                //收到服务器的数据
+                JObject json = JObject.Parse(data);
+                UserClient.DateTimeServer = json["Time"].ToObject<DateTime>();
+                List<string> chats = JArray.Parse(json["chats"].ToString()).ToObject<List<string>>();
+                StringBuilder sb = new StringBuilder();
+                chats.ForEach(m => { sb.Append(m + Environment.NewLine); });
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    //toolStripStatusLabel_time.Text = UserClient.DateTimeServer.ToString("yyyy-MM-dd HH:mm");
+                    //label_file_count.Text = json["FileCount"].ToObject<int>().ToString();
+                    //UIControls_Chat.AddChatsHistory(sb.ToString());
+                }));
+            }
+            else if (customer == CommonHeadCode.MultiNetHeadCode.文件总数量)
+            {
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    //label_file_count.Text = data;
+                }));
+            }
+            else if (customer == CommonHeadCode.MultiNetHeadCode.留言版消息)
+            {
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    //UIControls_Chat?.DealwithReceive(data);
+                }));
+            }
+        }
+
+        private void Net_socket_client_AcceptByte(AsyncStateOne object1, int customer, byte[] object2)
+        {
+            //接收到服务器发来的字节数据
+            Dispatcher.Invoke(new Action(() =>
+            {
+                MessageBox.Show(customer.ToString());
+            }));
+        }
+
+        private void Net_socket_client_LoginSuccess()
+        {
+            //登录成功，或重新登录成功的事件，有些数据的初始化可以放在此处
+            Dispatcher.Invoke(new Action(() =>
+            {
+                //toolStripStatusLabel_status.Text = "客户端启动成功";
+            }));
+        }
+
+        private void Net_socket_client_LoginFailed(int object1)
+        {
+            //登录失败的情况，如果连续三次连接失败，请考虑退出系统
+            if (object1 > 3)
+            {
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    Close();
+                }));
+            }
+        }
+
+        private void Net_socket_client_MessageAlerts(string object1)
+        {
+            //信息提示
+            Dispatcher.Invoke(new Action(() =>
+            {
+                //toolStripStatusLabel_status.Text = object1;
+            }));
+        }
+
+
+        #endregion
+
+
+
+
+
+
+
+
+
     }
 }
