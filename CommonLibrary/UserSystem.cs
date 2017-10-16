c2d2ac622cefbb585a3db18f1dd7453cb6304237
dathlin <hsl200909@163.com>
2017-10-16 10:34:43 +0800

    组件更新，服务器端网络新增发送指定别名客户端数据的功能，v1.6.16

--- CommonLibrary/UserSystem.cs
+++ CommonLibrary/UserSystem.cs
@@ -43,7 +43,7 @@ namespace CommonLibrary
              **************************************************************************/
 
 
-            SoftBasic.FrameworkVersion = new SystemVersion("1.6.15");
+            SoftBasic.FrameworkVersion = new SystemVersion("1.6.16");
 
         }
 
