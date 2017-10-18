2cd8989cc67392557a2b60a6f9c00293447a10de
dathlin <hsl200909@163.com>
2017-10-18 13:20:05 +0800

    组件更新，写入PLC数据时提供更加丰富的API，v1.6.20

--- CommonLibrary/UserSystem.cs
+++ CommonLibrary/UserSystem.cs
@@ -43,7 +43,7 @@ namespace CommonLibrary
              **************************************************************************/
 
 
-            SoftBasic.FrameworkVersion = new SystemVersion("1.6.19");
+            SoftBasic.FrameworkVersion = new SystemVersion("1.6.20");
 
         }
 
