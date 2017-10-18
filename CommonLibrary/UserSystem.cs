b450908e35cafc8581ffdd3cb2f72989771de6c2
dathlin <hsl200909@163.com>
2017-10-18 09:34:38 +0800

    组件更新，新增一个访问西门子的类，支持1200系列通讯，v1.6.19

--- CommonLibrary/UserSystem.cs
+++ CommonLibrary/UserSystem.cs
@@ -43,7 +43,7 @@ namespace CommonLibrary
              **************************************************************************/
 
 
-            SoftBasic.FrameworkVersion = new SystemVersion("1.6.18");
+            SoftBasic.FrameworkVersion = new SystemVersion("1.6.19");
 
         }
 
