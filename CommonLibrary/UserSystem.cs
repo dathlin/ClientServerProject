f9e44b3812567ed6843d6322e18204875273f9bf
dathlin <hsl200909@163.com>
2017-12-23 22:14:39 +0800

    winform客户端登录窗体图标更新，v1.7.10

--- CommonLibrary/UserSystem.cs
+++ CommonLibrary/UserSystem.cs
@@ -44,7 +44,7 @@ namespace CommonLibrary
              **************************************************************************/
 
 
-            SoftBasic.FrameworkVersion = new SystemVersion("1.7.9");
+            SoftBasic.FrameworkVersion = new SystemVersion("1.7.10");
 
         }
 
